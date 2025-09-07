14f6e79de7e8ddae8d753328975e6c2838ba9540
Vishy Gopal <112359514+VG-Fish@users.noreply.github.com>
2025-09-07 17:34:45 -0400

    finished updating email

--- emails/index.tsx
+++ emails/index.tsx
@@ -20,42 +20,27 @@ export const NotionWaitlistEmail = ({ userFirstname }: EmailProps) => (
     <Preview>Thanks for Joining the Waitlist, {userFirstname}! 🎉</Preview>
     <Body style={main}>
       <Container style={container}>
-        <Img
-          src={`https://nextjs-notion-waitlist.vercel.app/waitlist-logo.png`}
-          width="220"
-          height="100"
-          alt="Notion Waitlist Logo"
-          style={logo}
-        />
         <Text style={greeting}>Hi {userFirstname},</Text>
         <Text style={paragraph}>
-          Thanks for joining the waitlist for our Next.js + Notion CMS waitlist
-          template! I'm Lakshay, the developer behind this project. I'm glad to
-          have you on board.
+          Thanks for joining the waitlist for Matrix Edge Therapeutics! I'm Elaine, the CEO behind this startup.
+          We're glad to have you on board.
         </Text>
         <Text style={paragraph}>
-          I'll keep you posted on the progress and notify you as soon as it's
+          We'll keep you posted on the progress and notify you as soon as it's
           ready for you to use. In the meantime, if you have any questions or
           feedback, don't hesitate to reach out by replying directly to{" "}
-          <a href="mailto:lakshb.work@gmail.com" style={link}>
-            this email {""}
-          </a>
-          — I'm here to listen!
-        </Text>
-        <Text style={paragraph}>
-          You can also follow me on X/Twitter for updates:{" "}
-          <a href="https://x.com/blakssh" style={link}>
-            @blakssh
-          </a>
+          <a href="mailto:MatrixEdgeTherapeutics@gmail.com" style={link}>
+            this email{""}
+          </a>.
         </Text>
         <Text style={signOff}>
           Best regards,
           <br />
-          Lakshay
+          Elaine
         </Text>
         <Hr style={hr} />
         <Text style={footer}>
-          You received this email because you signed up for the Notion waitlist.
+          You received this email because you signed up for updates regarding Matrix Edge Therapeutics.
           If you believe this is a mistake, feel free to ignore this email.
         </Text>
       </Container>
